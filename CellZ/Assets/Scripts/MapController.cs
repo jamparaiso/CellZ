3ae6fc026fefb128625235c5428d2a92a892db65
John Michael Paraiso <johnmparaiso@gmail.com>
2023-11-15 13:54:06 +0000

    chunk trigger collider

--- CellZ/Assets/Scripts/MapController.cs
+++ CellZ/Assets/Scripts/MapController.cs
@@ -29,7 +29,7 @@ public class MapController : MonoBehaviour
     {
         if (!currentChunk)
         {
-            return;
+          return;
         }
 
         float xPos = joyStickController.xPos;
